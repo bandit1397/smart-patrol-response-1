3a54a05300ec1f27cc6be88be9b1499256540740
bandit <80446239+bandit1397@users.noreply.github.com>
2026-05-12 12:33:48 +0900

    Update patrolSchedule.js

--- patrolSchedule.js
+++ patrolSchedule.js
@@ -3,13 +3,18 @@ const patrolSchedules = {
   
   { id:"CV1(42호)", startDate:"2026-04-14", endDate:"2026-05-13", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"17:00", end:"18:00" }] },
     
+  { id:"CV2(41호)", startDate:"2026-05-07", endDate:"2026-06-06", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"23:00", end:"23:59" }] },
+    
  
     
   { id:"CV3(41호)", startDate:"2026-04-17", endDate:"2026-05-16", daysOfWeek:[1,3,5], timeRanges:[{ start:"14:00", end:"15:00" },{ start:"19:00", end:"20:00" }] },
 
+  { id:"CV4(43호)", startDate:"2026-05-11", endDate:"2026-06-10", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"08:00", end:"09:00" },{ start:"10:00", end:"11:00" }] },
+
+
     
        
-  { id:"CV6(41호)", startDate:"2026-04-15", endDate:"2026-05-14", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"20:00", end:"21:00" }] },
+  { id:"CV6(41호)", startDate:"2026-04-15", endDate:"2026-06-14", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"20:00", end:"21:00" }] },
     
     
   { id:"CV7(42호)", startDate:"2026-04-16", endDate:"2026-05-15", daysOfWeek:[1,2,3,4,5], timeRanges:[{ start:"14:00", end:"15:00" }] },
